061a720807afe0a985c181f21b695c06557e340e
faelvirn <cterrey019@gmail.com>
2026-09-03 21:09:01 +0000

    update 2

--- 000_Labs/006_doubles/base_code/starter.java
+++ 000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,8 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		double fahrenheit = 45.0;
+		double celsius = (fahrenheit-32)*5/9;
+		System.out.println("The celsius value for "  + fahrenheit + " degrees Fahrenheit is " + celsius + " degrees."); 
 	}
 }
--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -9,6 +9,19 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner input = new Scanner(System.in);
+		System.out.print("What is your name?"); 
+		String name = input.nextString();
+		System.out.print("How old are you?"); 
+		int age = input.nextInt();
+		System.out.print("What is the month you were born in?");
+		String month = input.nextString();
+		System.out.print("What day were your born on?");
+		int day = input.nextInt();
+		System.out.println("What year were your born?");
+		int year = input.nextInt();
+		System.out.print("How much is a buck fifty?"); 
+		double money = input.nextDouble();
+		System.out.println("you are" + name + ", " + age + "years old. You were born on " +);
 	}
 }
